664a0c95db80cffa8ef8c9bb29bfb73d6d07fb10
mightyblackbeard <josh.baca87@gmail.com>
2016-12-13 15:11:59 -0800

    In Chapter Swim Lessons
    the chapter exercise

--- ASSignments/ASSignments/Form1.cs
+++ ASSignments/ASSignments/Form1.cs
@@ -14,8 +14,45 @@ namespace ASSignments
     {
         public Form1()
         {
-            Text = "Some Title";
             InitializeComponent();
+            this.rbInt.Checked = true;
+        }
+
+        private void Compute_CheckedChanged(object sender, EventArgs e)
+        {
+            decimal total = 0;
+
+            if(cbSwim.Checked)
+            {
+                total += 25;
+            }
+            if (cbSnorkel.Checked)
+            {
+                total += 50;
+            }
+            if (cbDive.Checked)
+            {
+                total += 100;
+            }
+
+            if(rbBeg.Checked)
+            {
+                total += 10;
+                ExpMod.Text = "Beginner - $10 additional charge";
+            }
+            else
+            {
+                if(rbAdv.Checked)
+                {
+                    total -= 15;
+                    ExpMod.Text = "Advanced - $15 discount";
+                }
+                else
+                {
+                    ExpMod.Text = "";
+                }
+            }
+            tbTotal.Text = total.ToString("C");
         }
     }
 }
