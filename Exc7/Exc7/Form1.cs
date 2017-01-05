1e2c1c5d5c3d28668539c5ced930c683e613c52d
mightyblackbeard <josh.baca87@gmail.com>
2017-01-05 14:22:10 -0800

    modified
    Added "User Ability to Change Quantity"
    initialized price and rate onload()
    modified message box to reflect changes made

--- Exc7/Exc7/Form1.cs
+++ Exc7/Exc7/Form1.cs
@@ -20,38 +20,39 @@ namespace Exc7
             InitializeComponent();
         }
 
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            price = 0.00;
+            shipRate = 0.0;
+        }
+   
+
         private void lbBags_SelectedIndexChanged(object sender, EventArgs e)
         {
             switch(lbBags.SelectedIndex)
             {
                 case 0:
-                    price = 50.00;
                     tbSelected.Text = lbBags.SelectedItem.ToString() + " - $50.00";
                     break;
                 case 1:
-                    price = 45.00;
                     tbSelected.Text = lbBags.SelectedItem.ToString() + " - $45.00";
                     break;
                 case 2:
-                    price = 40.00;
                     tbSelected.Text = lbBags.SelectedItem.ToString() + " - $40.00";
                     break;
                 case 3:
-                    price = 25.00;
                     tbSelected.Text = lbBags.SelectedItem.ToString() + " - $25.00";
                     break;
                 case 4:
-                    price = 30.00;
                     tbSelected.Text = lbBags.SelectedItem.ToString() + " - $30.00";
                     break;
                 case 5:
-                    price = 20.00;
                     tbSelected.Text = lbBags.SelectedItem.ToString() + " - $20.00";
                     break;
                 default:
                     break;
             }
-           
+            nudQuantity.Value = 1;
             bClear.Enabled = true;
         }
 
@@ -59,32 +60,63 @@ namespace Exc7
         {
             tbSelected.Text = "";
             price = 0.00;
+            nudQuantity.Value = 0;
             bClear.Enabled = false;
         }
 
         private void bOrder_Click(object sender, EventArgs e)
         {
+            switch (lbBags.SelectedIndex)
+            {
+                case 0:
+                    price = 50.00;
+                    break;
+                case 1:
+                    price = 45.00;
+                    break;
+                case 2:
+                    price = 40.00;
+                    break;
+                case 3:
+                    price = 25.00;
+                    break;
+                case 4:
+                    price = 30.00;
+                    break;
+                case 5:
+                    price = 20.00;
+                    break;
+                default:
+                    break;
+            }
+
+            price *= (double)nudQuantity.Value;     
+
             if (rbOvernight.Checked)
             {
                 shipRate = .10;
-                MessageBox.Show("YOUR ORDER: \n\n " + tbSelected.Text + "\n" + rbOvernight.Text +
-                    ":  " + (price * shipRate).ToString("C") + "\n\n Total: " + (price + (price * shipRate)).ToString("C"), "Order",
+                MessageBox.Show("YOUR ORDER: \n\n " + lbBags.SelectedItem + "(" + nudQuantity.Value.ToString() + ")" +
+                    " - " + price.ToString("C") + "\n" + rbOvernight.Text + ":  " + (price * shipRate).ToString("C") + 
+                    "\n\n Total: " + (price + (price * shipRate)).ToString("C"), "Order",
                     MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
             else if(rbThree.Checked)
             {
                 shipRate = .07;
-                MessageBox.Show("YOUR ORDER: \n\n " + tbSelected.Text + "\n" + rbThree.Text +
-                    ":  " + (price * shipRate).ToString("C") + "\n\n Total: " + (price + (price * shipRate)).ToString("C"), "Order",
+                MessageBox.Show("YOUR ORDER: \n\n " + lbBags.SelectedItem + "(" + nudQuantity.Value.ToString() + ")" +
+                    " - " + price.ToString("C") + "\n" + rbThree.Text + ":  " + (price * shipRate).ToString("C") + 
+                    "\n\n Total: " + (price + (price * shipRate)).ToString("C"), "Order",
                     MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
             else
             {
                 shipRate = .05;
-                MessageBox.Show("YOUR ORDER: \n\n " + tbSelected.Text + "\n" + rbStandard.Text +
-                    ":  " + (price * shipRate).ToString("C") + "\n\n Total: " + (price + (price * shipRate)).ToString("C"), "Order",
+                MessageBox.Show("YOUR ORDER: \n\n " + lbBags.SelectedItem + "(" + nudQuantity.Value.ToString() + ")"+
+                    " - " + price.ToString("C") + "\n" + rbStandard.Text + ":  " + (price * shipRate).ToString("C") + 
+                    "\n\n Total: " + (price + (price * shipRate)).ToString("C"), "Order",
                     MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
             }
         }
+
     }
 }
